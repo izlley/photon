6fc54bd786a6fc08ac89c7a0e8b69790eb546069
1000632 <leejy@ubuntu.ubuntu-domain>
2013-11-07 15:16:01 +0900

    initialize

---
+++ positron/workqueue.h
@@ -0,0 +1,50 @@
+
+#ifndef WORKQUEUE_H
+#define WORKQUEUE_H
+
+#include <pthread.h>
+
+typedef struct worker {
+    pthread_t thread;
+    int terminate;
+	int runCount;
+    struct workqueue *workqueue;
+    struct worker *prev;
+    struct worker *next;
+} worker_t;
+
+typedef struct job {
+    void (*jobFunction)(struct job *job);
+    void *userData;
+    struct job *prev;
+    struct job *next;
+} job_t;
+
+typedef struct freejob {
+  struct job *freeJobs;
+  pthread_mutex_t freeJobsMutex;
+} freejob_t;
+
+typedef struct workqueue {
+    struct worker *workers;
+    struct job *waitingJobs;
+	struct freejob sfreeJob;
+	int freeCount;
+	int maxFreeCnt;
+	int waitCount;
+	int runCountSum;
+    pthread_mutex_t jobsMutex;
+    pthread_cond_t jobsCond;
+} workqueue_t;
+
+int workqueue_init(workqueue_t *aWorkqueue, int aNumWorkers);
+
+void workqueue_shutdown(workqueue_t *aWorkqueue);
+
+void workqueue_add_job(workqueue_t *aWorkqueue, job_t *aJob);
+
+void wqFreeJob(workqueue_t *aWorkqueue, job_t *aJob);
+
+void wqAllocJob(workqueue_t *aWorkqueue, job_t *aJob);
+
+#endif  /* #ifndef WORKQUEUE_H */
